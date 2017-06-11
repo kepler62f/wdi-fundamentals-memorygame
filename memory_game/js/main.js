9ca3af14f59f15db50a096e5087374260f0eeb05
kepler62f <blue.kepler62f@gmail.com>
2017-06-11 15:39:23 +0200

    finalize game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -23,26 +23,59 @@ var cards = [
 ];
 var cardsInPlay = [];
 
-var checkForMatch = function() {
+var checkForMatch = function(k, click_item) {
+	//unveil the latest card clicked on
+	click_item.setAttribute('src', cards[k].cardImage);
 	if (cardsInPlay.length === 2) {
-		if (cardsInPlay[0] === cardsInPlay[1]) {
-			alert("You found a match!");
+		first_click = cardsInPlay[0];
+		second_click = cardsInPlay[1];
+		if (first_click != second_click) {
+			if (cards[first_click.getAttribute('data-id')].rank === cards[second_click.getAttribute('data-id')].rank) {    
+				alert("You found a match!");
+			} else {
+				alert("Sorry, try again.");
+			}
+			//cover up cards and reset game 	
+			cardsInPlay[0].setAttribute('src', 'images/back.png');
+			cardsInPlay[1].setAttribute('src', 'images/back.png');
+			cardsInPlay = [];
 		} else {
-			alert("Sorry, try again.");
+			// clear away the second click from cardsInPlay because it's the same as first click 
+			cardsInPlay.pop();
 		}
 	}
 }
 
-var flipcard = function(cardId) {
+var flipcard = function() {
+	console.log("flip ", this);
+	var cardId = this.getAttribute('data-id');
 	console.log("User flipped " + cards[cardId].rank);
-	console.log(cards[cardId].cardImage);
-	console.log(cards[cardId].suit);
-	cardsInPlay.push(cards[cardId].rank); 	
-	checkForMatch();
+	cardsInPlay.push(this); 	
+	checkForMatch(cardId, this);
 }
 
-flipcard(0)
-flipcard(2)
+var createBoard = function() {
+	for (var i = 0; i < cards.length; i++) {
+		var cardElement = document.createElement('img');
+		cardElement.setAttribute('src', 'images/back.png');
+		cardElement.setAttribute('data-id', i);
+		cardElement.addEventListener('click', flipcard);
+		document.getElementById('game-board').appendChild(cardElement);
+	}
+}
+
+createBoard();
 
 
+// var checkForMatch = function(k, click_item) {
+// 	click_item.setAttribute('src', cards[k].cardImage);
+// 	if (cardsInPlay.length === 2) {
+// 		if (cards[cardsInPlay[0]].rank === cards[cardsInPlay[1]].rank) {    
+// 			alert("You found a match!");
+// 		} else {
+// 			alert("Sorry, try again.");
+// 		}
 
+// 		cardsInPlay = [];
+// 	}
+// }
